05f26abb0d69c471d580c9b4fd96c2999e1f07d5
Emily Dalton <emilydalton@dhcp-10-105-205-249.wireless.northwestern.private>
2018-10-20 15:28:06 -0500

    adding validated html, psuedocoded javascript. HTML validation had a problem saying I had a stray <script> tag, but I couldn't find it.

--- assets/guess.js
+++ assets/guess.js
@@ -7,46 +7,35 @@ console.log("this is your ramdom letter:  " + randomLetter);
 var reSet;
 var userGuess;
 var guessArray = []
-// window.addEventListener("keypress", display);
-
-//  reSet = document.getElementById("reSet")
-//  document.addEventListener("click",reset);
-
-// function gameInit(){
-
+//collects the keystroke that represents the user guess 
 document.onkeypress = function (event) {
     userGuess = event.key;
     console.log("your guess was" + " " + userGuess);
+//puts the user guess into an array 
     guessArray.push(" " + userGuess);
     console.log("this is your new array" + guessArray)
     console.log("you have this many guesses remaining" + " " + guesses);
 
-    //during the time that losses do not ammount to 1 
-    // while (losses < 1 && wins < 1) {
-    //if the user guess input equals the  random letter increment the wins by 1 and congratulate
-    if (userGuess === randomLetter) {
+    
+    //if the user guess input equals the  random letter increment the wins by 1 
+    if (userGuess === randomLetter && guesses > 0) {
         wins++;
         console.log("you have this many wins" + " " + wins);
-        // if input incorrect decrement the number of guesses 
-    } else if (userGuess !== randomLetter) {
+        // else if input does not equal the user guess the number of guesses are larger than zero decrement the number of guesses 
+    } else if (userGuess !== randomLetter && guesses > 0) {
         guesses--;
         console.log("you have this many guesses left" + "  " + guesses);
-    } else if (guesses === 0) {
-        losses++;
-        console.log("you have lost this many times: " + " " + losses);
-
+        // else the above conditions are not true, meaning the guess was either correct,  incorrect and remaining guesses are greater than zero  
     } else {
-        wins =0; 
-        losses = 0; 
+        losses++;
+        guesses = 0;
         guessArray = [];
-
     }
 
 
-    //should return new random letter
+    //function attached to a button with on click event that picks a new random letter , resets guesses and empties guess array should be here. 
 
-
-    //  function display(){
+    // displays the the letters, attempts, wins, and losses 
 
     document.getElementById("letterTrack").textContent = " " + guessArray;
     document.getElementById("attempts").textContent = guesses;
